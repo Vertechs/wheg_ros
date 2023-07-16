b1904ee53846a7017d6c6d0003dc44da9433dc0d
Demo User <debian@beaglebone.localdomain>
2023-07-16 05:29:40 +0000

    can scripts

--- wheg/src/scripts/odrive_utils/CAN_test.py
+++ wheg/src/scripts/odrive_utils/CAN_test.py
@@ -13,6 +13,8 @@ else:
 
 axisID = 0xC
 
+heartbeatID = axisID <<5 | db.get_message_by_name('Heartbeat').frame_id
+
 print("send axis calib 0x03 to "+str(axisID))
 
 msg = db.get_message_by_name('Set_Axis_State')
@@ -22,3 +24,32 @@ data = msg.encode({'Axis_Requested_State':0x03})
 msg = can.Message(arbitration_id=msg.frame_id | axisID << 5, is_extended_id=False, data=data)
 print(db.decode_message('Set_Axis_State',msg.data))
 print(msg)
+
+try:
+    bus.send(msg)
+    print("msg sent on {}".format(bus.channel_info))
+except can.CanError:
+    print("msg failed")
+    
+while True:
+    msg = bus.recv()
+    # check if arb ID is from correct axis or'ed with heartbeat message ID
+    if msg.arbitration_id == ((axisID << 5) | db.get_message_by_name('Heartbeat').frame_id):
+        # decode heartbeat message and extract axis state bits
+        current_state = db.decode_message('Heartbeat',msg.data)['Axis_State']
+        if current_state == 0x1:
+            print("idling")
+            break
+    
+# can also iterate over bus to get messsages
+for msg in bus:
+    if msg.arbitration_id == heartbeatID:
+        errCode = db.decode_message('Heartbeat',msg.data)['Axis_Error']
+        if errCode == 0x00:
+            print("no error")
+        else:
+            print("AXIS ERROR: ",str(hex(errCode)))
+        break
+    
+    
+bus.shutdown()
