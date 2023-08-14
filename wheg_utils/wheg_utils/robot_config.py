f9da8d2e053aee4935168268c4f68698f095130d
Demo User <tybi51398@gmail.com>
2023-08-14 04:53:13 +0000

    updated config, added walk controller

--- wheg_utils/wheg_utils/robot_config.py
+++ wheg_utils/wheg_utils/robot_config.py
@@ -1,18 +1,56 @@
-from whegs_4bar import WhegFourBar
+from wheg_utils.whegs_4bar import WhegFourBar
 
 # storing robot configs as python classes instead of in ini file
 
+# wheel module numbering is L->R, T->B 
+##   (0) O==||==O (1)
+##          ||
+##          ||
+##   (2) O==||==O (3)
+
 class RobotDefinition:
-    def __init__(self,wheels):
+    def __init__(self,num_wheels):
         self.num_wheels = num_wheels
         
-        # Hardware variables
-        self.drive_sn = ['']*self.num_wheels
+        ## Set variables ##
         
-        # Mechanical variables
+        # Odrive serial numbers for USB comms
+        self.drive_sn = [''] * self.num_wheels
+        
+        # Odrive axis CAN IDs
+        self.axis_ids = [0x00] * self.num_wheels * 2
+        
+        # set up module objects for each wheel/drive
         self.modules = []
-        for i in range(self.num_wheels)
+        for i in range(self.num_wheels):
             self.modules.append(WheelDefinition())
+            
+        # Wheel base length and width (mm) and offset (x,y,z)(mm) from robot center of mass
+        self.wheel_base_offset = (0.0,0.0,0.0)
+        self.wheel_base_length = 100
+        self.wheel_base_width  = 100
+        
+        ## Calculated variables ##        
+    
+        # wheel center positions (x,y,z)(mm) relative to robot center of mass
+        self.centers = [(0.0,0.0,0.0)] * self.num_wheels
+        
+        
+    # set wheel center positions assuming a rectangular wheel base 
+    def set_centers(self):
+        for i in range(self.num_wheels):
+            # Odd numbered wheels are on the right (y-)
+            # wheels >2 are on back (x-)
+            d,s=1,1
+            if i%2 != 0: d = -1
+            if i>2: s = -1
+            
+            x = s * self.wheel_base_length / 2
+            y = d * self.wheel_base_width / 2
+            o = self.wheel_base_offset
+            
+            self.centers[i] = (x+o[0],y+o[1],o[2])
+        
         
         
 # wheel gear ratios
@@ -53,28 +91,35 @@ def get_config_A():
     # odrive serial numbers in order
     robcfg.drive_sn = ['208839824D4D', '205839844D4D', '209039854D4D', '205239824D4D']
     
+    robcfg.axis_ids = [a for a in range(0x0A,0x12)]
+    
+    robcfg.wheel_base_length = 12.2 * 25.4
+    robcfg.wheel_base_width = 20.2 * 25.4
+    robcfg.wheel_base_offset = (0,0,1.25*25.4)
+    robcfg.set_centers()
+    
     robcfg.modules[0].outer_stages = [(60,20),(60,20)]
     robcfg.modules[0].inner_stages = [(60,20),(60,20)]
     robcfg.modules[0].wheel_stages = [(1,1)]
     robcfg.modules[0].set_ratios()
     robcfg.modules[0].n_arc = 5
     
-    robcfg.modules[0].outer_stages = [(40,14),(40,14)]
-    robcfg.modules[0].inner_stages = [(40,14),(40,14)]
-    robcfg.modules[0].wheel_stages = [(1,1)]
-    robcfg.modules[0].set_ratios()
-    robcfg.modules[0].n_arc = 5
+    robcfg.modules[1].outer_stages = [(40,14),(40,20)]
+    robcfg.modules[1].inner_stages = [(40,14),(40,20)]
+    robcfg.modules[1].wheel_stages = [(1,1)]
+    robcfg.modules[1].set_ratios()
+    robcfg.modules[1].n_arc = 5
     
-    robcfg.modules[0].outer_stages = [(40,14),(40,14)]
-    robcfg.modules[0].inner_stages = [(40,14),(40,14)]
-    robcfg.modules[0].wheel_stages = [(24+82,24)] # S / (R+S) for planetary
-    robcfg.modules[0].set_ratios()
-    robcfg.modules[0].n_arc = 5
+    robcfg.modules[2].outer_stages = [(40,14),(40,20)]
+    robcfg.modules[2].inner_stages = [(40,14),(40,20)]
+    robcfg.modules[2].wheel_stages = [(24+82,24)] # S / (R+S) for planetary
+    robcfg.modules[2].set_ratios()
+    robcfg.modules[2].n_arc = 5
     
-    robcfg.modules[0].outer_stages = [(40,14),(40,14)]
-    robcfg.modules[0].inner_stages = [(40,14),(40,14)]
-    robcfg.modules[0].wheel_stages = [(32+80,32)] # S / (R+S) for planetary
-    robcfg.modules[0].set_ratios()
-    robcfg.modules[0].n_arc = 5
+    robcfg.modules[3].outer_stages = [(40,14),(40,14)]
+    robcfg.modules[3].inner_stages = [(40,14),(40,14)]
+    robcfg.modules[3].wheel_stages = [(32+80,32)] # S / (R+S) for planetary
+    robcfg.modules[3].set_ratios()
+    robcfg.modules[3].n_arc = 5
     
     return robcfg
